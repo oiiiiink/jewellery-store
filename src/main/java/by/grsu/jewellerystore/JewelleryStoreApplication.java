b9239ba9a2bfa5711522598d0973c18f3bcba732
oiiiiink <maksim_samsanovich@senla.eu>
2022-04-06 12:35:07 +0300

    project environment

---
+++ src/main/java/by/grsu/jewellerystore/JewelleryStoreApplication.java
@@ -0,0 +1,13 @@
+package by.grsu.jewellerystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelleryStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelleryStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/grsu/jewellerystore/JewelleryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package by.grsu.jewellerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
