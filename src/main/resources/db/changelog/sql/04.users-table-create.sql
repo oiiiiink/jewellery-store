9d2febf392991c3dc0ce54c1031aca65ed05cae3
oiiiiink <maksim_samsanovich@senla.eu>
2022-04-22 17:51:03 +0300

    dtos

--- src/main/resources/db/changelog/sql/04.users-table-create.sql
+++ src/main/resources/db/changelog/sql/04.users-table-create.sql
@@ -5,5 +5,6 @@ create table users (
     first_name varchar(25) not null,
     phone_number varchar(25) not null,
     delivery_address varchar(100) not null,
-    roles_id bigint references roles(id)
+    roles_id bigint not null,
+    foreign key (roles_id) references roles(id)
 );
